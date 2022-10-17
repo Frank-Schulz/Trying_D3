6d7255e460e354a5f0a929fe34357cd5292c712c
Frank Schulz <frank.schulz537@gmail.com>
2022-10-17 17:50:56 +1300

    updated routes and controllers to handle  on demand loading

--- first steps/backend/routes/index.routes.js
+++ first steps/backend/routes/index.routes.js
@@ -3,12 +3,18 @@ var router = express.Router();
 
 const {
   getIndex,
+  getRoot,
+  getChildrenOf,
 } = require('../controllers/index.controller');
 
-/* GET home page. */
+// GET home page.
 router.get('/', getIndex);
 
+// GET root node
+router.get('/root', getRoot)
 
+// POST gets the children of the given person
+router.post('/children', getChildrenOf)
 
 
 module.exports = router;
